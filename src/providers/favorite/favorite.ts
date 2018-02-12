c4bf9c7dfb8bc1f7a53e4fd470eeff734d87674b
Saumya <saumya.shekhar@sap.com>
2018-02-12 19:53:17 +0530

    FAB Favorite

---
+++ src/providers/favorite/favorite.ts
@@ -0,0 +1,29 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+
+/*
+  Generated class for the FavoriteProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class FavoriteProvider {
+
+  favorites: Array<any>;
+
+  constructor(public http: Http) {
+    console.log('Hello FavoriteProvider Provider');
+    this.favorites = [];
+  }
+
+  addFavorite(id: number): boolean {
+    this.favorites.push(id);
+    return true;
+  }
+
+  isFavorite(id: number): boolean {
+    return this.favorites.some(el => el === id);
+  }
+}
