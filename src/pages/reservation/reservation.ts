b2832d377442609a6237d79cae5b4d9e372c782f
Saumya <saumya.shekhar@sap.com>
2018-02-24 12:30:31 +0530

    Forms

--- src/pages/reservation/reservation.ts
+++ src/pages/reservation/reservation.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
+import { Validators, FormBuilder, FormGroup } from '@angular/forms';
 
 /**
  * Generated class for the ReservationPage page.
@@ -15,8 +16,17 @@ import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angul
 })
 export class ReservationPage {
 
+  reservation: FormGroup;
+
   constructor(public navCtrl: NavController, public navParams: NavParams,
-    public viewCtrl: ViewController) {
+    public viewCtrl: ViewController,
+    private formBuilder: FormBuilder) {
+
+    this.reservation = this.formBuilder.group({
+      guests: 3,
+      smoking: false,
+      dateTime: ['', Validators.required],
+    });
   }
 
   ionViewDidLoad() {
@@ -27,4 +37,8 @@ export class ReservationPage {
     this.viewCtrl.dismiss();
   }
 
+  onSubmit() {
+    console.log(this.reservation.value);
+    this.viewCtrl.dismiss();
+  }
 }
